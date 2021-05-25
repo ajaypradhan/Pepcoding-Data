829cf27b79a95e0b5cb52dc109f0a74b4fea06e9
ajaypradhan <pradhanajay14@gmail.com>
2021-05-26 01:13:00 +0530

    organize done

--- WD_projects_YT/FileOrganizer/main.js
+++ WD_projects_YT/FileOrganizer/main.js
@@ -1,4 +1,8 @@
+const fs = require("fs");
+const path = require("path");
+
 let inputArr = process.argv.slice(2);  //take input from command line
+
 // console.log(inputArr);
 //node main.js tree "directory path"
 //node main.js organize "directory path"
@@ -23,9 +27,36 @@ switch (command) {
 function treeFn(dirPath) {
     console.log("Tree command implemented for ", dirPath);
 }
+
+
+//organize function
 function organizeFn(dirPath) {
-    console.log("Organize command implemented for ", dirPath);
+    // console.log("Organize command implemented for ", dirPath);
+    
+    // 1. input -> directory path
+    if(dirPath == undefined){
+        console.log("Kindly enter the path");
+    }else{
+        let doesExist = fs.existsSync(dirPath); //it check if the directory exist or not
+        if(dirPath){
+            // 2. create - > organized_file -> directory
+            let destPath = path.join(dirPath,"organized_files"); //destination path
+            if(fs.existsSync(destPath) == false){ //check if the file exist or not
+                fs.mkdirSync(destPath); //makes an folder 
+            }
+        }else{
+            console.log("Kindly enter the path");
+            return;
+        }
+    }
+    // 3. identify category of all the files present in that input directory
+    // 4/ copy/ cut files to that organized directory inside of any category folder
+    
 }
+
+
+
+//help function
 function helpFn() {
     console.log(`
     List of All the commands:
