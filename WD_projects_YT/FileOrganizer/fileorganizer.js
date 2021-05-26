bf52b81a33d71f47d46eb676d4563199278e8486
ajaypradhan <pradhanajay14@gmail.com>
2021-05-26 15:11:00 +0530

    command folder add and project complete

--- WD_projects_YT/FileOrganizer/fileorganizer.js
+++ WD_projects_YT/FileOrganizer/fileorganizer.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 const fs = require("fs");
 const path = require("path");
+const helpObj = require("./commands/help");
+const treeObj = require("./commands/tree");
+const organizeObj = require("./commands/organize");
 
 let inputArr = process.argv.slice(2);
 // console.log(inputArr);
@@ -17,132 +20,15 @@ let types = {
 
 switch (command) {
     case "tree":
-        treeFn(inputArr[1]);
+        treeObj.treeKey(inputArr[1]);
         break;
     case "organize":
-        organizeFn(inputArr[1]);
+        organizeObj.organizeKey(inputArr[1]);
         break;
     case "help":
-        helpFn();
+        helpObj.helpKey();
         break;
     default:
         console.log("Please input correct command");
         break;
-}
-
-function treeFn(dirPath) {
-    if (dirPath == undefined) {
-        // console.log("Kindly enter the correct path");
-        treeHelper(process.cwd(), "");
-        return;
-    } else {
-        let doesExist = fs.existsSync(dirPath);
-        if (doesExist) {
-            treeHelper(dirPath, "");
-        } else {
-            console.log("Kindly enter the path");
-            return;
-        }
-    }
-}
-
-function treeHelper(dirPath, indent) {
-    //is file or folder
-    let isFile = fs.lstatSync(dirPath).isFile();
-    //if file print
-    if(isFile == true){
-        let fileName  = path.basename(dirPath);
-        console.log(indent + "|------" + fileName);
-    }else{
-        let dirName = path.basename(dirPath)
-        console.log(indent + "|______"+ dirName);
-        let children =fs.readdirSync(dirPath)
-        for(let i = 0; i < children.length; i++){
-            let childPath = path.join(dirPath,children[i]);
-            treeHelper(childPath,indent+"\t");
-        }
-    }
-    // if folder go into the folder
-
-}
-
-
-function organizeFn(dirPath) {
-    // 1. input -> directory path
-    let destPath;
-    if (dirPath == undefined) {
-        // console.log("Kindly enter the correct path");
-         destPath = process.cwd()
-        return;
-    } else {
-        let doesExist = fs.existsSync(dirPath);
-        if (doesExist) {
-            // 2. creat -> organized folder -> directory
-            destPath = path.join(dirPath, "organized_files");
-            if (fs.existsSync(destPath) == false) {
-                fs.mkdirSync(destPath);
-                console.log("organized folder created");
-            }
-        } else {
-            console.log("Kindly enter the path");
-            return;
-        }
-    }
-    organizeHelper(dirPath, destPath);
-
-}
-
-
-function organizeHelper(src, dest) {
-    // 3. identify the category of all the files in the input directory
-    let childNames = fs.readdirSync(src);
-    // console.log(childNames);
-    for (let i = 0; i < childNames.length; i++) {
-        let childAddress = path.join(src, childNames[i]);
-        let isFile = fs.lstatSync(childAddress).isFile();
-        if (isFile) {
-            // console.log(childNames[i]);
-            let category = getCategory(childNames[i]);
-            console.log(childNames[i], " belong to -->  ", category);
-            // 4. copy /  cut files to that organized directory inside of any category folder
-            sendFiles(childAddress, dest, category);
-        }
-    }
-}
-
-function getCategory(name) {
-    let ext = path.extname(name);
-    ext = ext.slice(1);
-    // console.log(ext);
-    for (let type in types) {
-        let cTypeArray = types[type];
-        for (let i = 0; i < cTypeArray.length; i++) {
-            if (ext == cTypeArray[i]) {
-                return type;
-            }
-        }
-    }
-    return "other";
-}
-
-function sendFiles(srcFilePath, dest, category) {
-    let categoryPath = path.join(dest, category);
-    if (fs.existsSync(categoryPath) == false) {
-        fs.mkdirSync(categoryPath);
-    }
-    let fileName = path.basename(srcFilePath)
-    let destFilePath = path.join(categoryPath, fileName);
-    fs.copyFileSync(srcFilePath, destFilePath);
-    fs.unlinkSync(srcFilePath);
-    console.log(fileName, "copied to ", category);
-}
-
-
-function helpFn() {
-    console.log(`
-    List of All the commands:
-                    node main.js tree "directory path"
-                    node main.js organize "directory path"
-                    node main.js help
-    `);
 }
