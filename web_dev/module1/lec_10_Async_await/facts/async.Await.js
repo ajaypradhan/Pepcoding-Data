b124f334d68235f56b2e0b22cf8938bc75941541
ajaypradhan <pradhanajay14@gmail.com>
2021-05-26 19:59:47 +0530

    using async wait log in

---
+++ web_dev/module1/lec_10_Async_await/facts/async.Await.js
@@ -0,0 +1,9 @@
+//async => it can bw used before a function name
+//await => it can only be used inside a async function
+
+//IIFE => Immediately Invoke Function Expressions
+
+const fs = require("fs");
+
+console.log("Start");
+
