71edb8336e1b00e78307d5525f8e7586df2f4c25
Ajay Pradhan <pradhanajay14@gmail.com>
2021-06-21 20:19:03 +0530

    tool options added

--- web_dev/module2/white-board/activity/tools.js
+++ web_dev/module2/white-board/activity/tools.js
@@ -3,6 +3,7 @@ let redo = document.querySelector("#redo");
 undo.addEventListener("click" , undoLine);
 redo.addEventListener("click" , redoLine);
 
+
 function undoLine(){
     if(linesDB.length){
         let undoLine = linesDB.pop();
@@ -14,7 +15,6 @@ function undoLine(){
         drawLinesFromDB();
     }
 }
-
 function redoLine(){
     if(redoLinesDB.length){
         let redoLine = redoLinesDB.pop();
@@ -31,11 +31,7 @@ function redoLine(){
         }
         linesDB.push(redoLine);
     }
-
-
-
 }
-
 function drawLinesFromDB(){
     for(let i=0 ; i<linesDB.length ; i++){
         let line = linesDB[i];
