fa7931143b69b965d5fd3d0dbe8160749046336e
Ajay Pradhan <pradhanajay14@gmail.com>
2021-06-29 23:52:59 +0530

    db initialized

--- web_dev/module2/excle-clone/activity/init.js
+++ web_dev/module2/excle-clone/activity/init.js
@@ -1,11 +1,14 @@
 let cellsContainer = document.querySelector(".cells");
+let db;
 
 function initCells() {
   let cellsContent = '<div class="top-left-cell"></div>';
 
   cellsContent += '<div class="top-row">';
   for (let j = 0; j < 26; j++) {
-    cellsContent += `<div class="top-row-cell">${String.fromCharCode(65+j)}</div>`;
+    cellsContent += `<div class="top-row-cell">${String.fromCharCode(
+      65 + j
+    )}</div>`;
   }
   cellsContent += "</div>";
 
@@ -14,16 +17,37 @@ function initCells() {
     cellsContent += `<div class="left-col-cell">${j + 1}</div>`;
   }
   cellsContent += "</div>";
-
   cellsContent += '<div class="all-cells">';
   for (let i = 0; i < 100; i++) {
     cellsContent += '<div class="row">';
     for (let j = 0; j < 26; j++) {
-      cellsContent += `<div class="cell" contenteditable="true"></div>`;
+      cellsContent += `<div class="cell" contenteditable="true" rowid="${i}" colid="${j}"></div>`;
     }
     cellsContent += "</div>";
   }
   cellsContent += "</div";
   cellsContainer.innerHTML = cellsContent;
 }
+
+function initDB() {
+  db = [];
+  for (let i = 0; i < 100; i++) {
+    let row = [];
+    for (let j = 0; j < 26; j++) {
+      // i=1
+      // j=1   =>   B2
+      let cellName = String.fromCharCode(65 + j) + (i + 1);
+      let cellObject = {
+        name: cellName,
+        value: "",
+        formula: "",
+      };
+      row.push(cellObject);
+    }
+    db.push(row);
+  }
+  console.log(db);
+}
+
 initCells();
+initDB();
