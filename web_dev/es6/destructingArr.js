32f64f899e3e07e23dda563db8491312a3679f92
Ajay Pradhan <pradhanajay14@gmail.com>
2021-08-02 20:46:27 +0530

    destructing

---
+++ web_dev/es6/destructingArr.js
@@ -0,0 +1,8 @@
+let a = [1, 2, 3, 4, 5];
+
+let [kuchBhi, , nextVar, _, reqVar] = a;
+//for multiple skip use , for one skip use _
+
+console.log(kuchBhi);
+console.log(nextVar);
+console.log(reqVar);
---
+++ web_dev/es6/destructureObj.js
@@ -0,0 +1,11 @@
+let add = {
+    city: 'Delhi',
+    region: 'Pritampura',
+    pin: 11001,
+};
+
+let { city, pin, region } = add;
+
+console.log(region);
+console.log(pin);
+console.log(city);
