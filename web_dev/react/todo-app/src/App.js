83f2897a8cca5334cfa3fa19491d8410d4ff7e10
Ajay Pradhan <pradhanajay14@gmail.com>
2021-08-03 21:38:42 +0530

    todo app done

---
+++ web_dev/react/todo-app/src/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+    state = {
+        tasks: ['make coffee', 'make notes', 'go for a jog'],
+        currInput: '',
+    };
+
+    render = () => {
+        return (
+            <div>
+                <input
+                    className="input-box"
+                    type="text"
+
+                    onChange={(e) => {
+                        this.setState({ currInput: e.currentTarget.value });
+                        console.log(e.currentTarget.value);
+                    }}
+                    onKeyDown={(e) => {
+                        if (e.key === 'Enter') {
+                            this.setState({
+                                tasks: [
+                                    ...this.state.tasks,
+                                    this.state.currInput,
+                                ],
+                                currInput: '',
+                            });
+                        }
+                    }}
+                    value={this.state.currInput}
+                    spellcheck="false"
+                />
+
+                <ul>
+                    {this.state.tasks.map((el) => {
+                        return <li>{el}</li>;
+                    })}
+                </ul>
+            </div>
+        );
+    };
+}
+
+export default App;
