26ad785bb6bf10609fe6233fc3aaeec5229b91b6
Ajay Pradhan <pradhanajay14@gmail.com>
2021-08-16 21:11:15 +0530

    firebase

---
+++ web_dev/react/context-api/src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: 'AIzaSyCbYUWI7lDeNZYz8REe2jzVWOABZkOUm90',
+    authDomain: 'context-api-test.firebaseapp.com',
+    projectId: 'context-api-test',
+    storageBucket: 'context-api-test.appspot.com',
+    messagingSenderId: '483935017338',
+    appId: '1:483935017338:web:0bdaf1543153eec2455ec3',
+    measurementId: 'G-Q9X4VMG6ZD',
+};
+
+firebase.initializeApp();
