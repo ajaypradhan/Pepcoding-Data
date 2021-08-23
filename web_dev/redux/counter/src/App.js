015900632abe2152013c5b34cc291d40bd748123
Ajay Pradhan <pradhanajay14@gmail.com>
2021-08-23 20:36:14 +0530

    multiple reducers added

--- web_dev/redux/counter/src/App.js
+++ web_dev/redux/counter/src/App.js
@@ -4,6 +4,7 @@ import { incrementCreator, decrementCreator } from "./redux/actions";
 
 let App = () => {
   let state = useSelector(function (state) {
+    console.log(state);
     return state;
   });
 
@@ -11,21 +12,28 @@ let App = () => {
 
   return (
     <>
-      <button
+      {/* <button
         onClick={() => {
-          dispatch(incrementCreator());
+          dispatch(incrementCreator(1));
         }}
       >
-        +
+        +1
       </button>
+      <button
+        onClick={() => {
+          dispatch(incrementCreator(10));
+        }}
+      >
+        +10
+      </button>
+      <p>{state}</p>
       <button
         onClick={() => {
           dispatch(decrementCreator());
         }}
       >
         -
-      </button>
-      <p>{state}</p>
+      </button> */}
     </>
   );
 };
--- web_dev/redux/counter/src/redux/reducer.js
+++ web_dev/redux/counter/src/redux/reducer.js
@@ -1,13 +1,23 @@
-let reducer = (state = 0, action) => {
-  switch (action.type) {
-    case "INCREMENT":
-      return state + 1;
-
-    case "DECREMENT":
-      return state - 1;
-    default:
-      return state;
-  }
-};
-
-export default reducer;
+export const counterReducer = (state = 0, action) => {
+    switch (action.type) {
+      case "INCREMENT":
+        return state + action.payload;
+  
+      case "DECREMENT":
+        return state - 1;
+  
+      default:
+        return state;
+    }
+  };
+  
+  export const loginReducer = (state = false, action) => {
+    switch (action.type) {
+      case "LOGIN":
+        return true;
+      case "LOGOUT":
+        return false;
+      default:
+        return state;
+    }
+  };
