59a5c7749773d1b91ed243d146d9b8c663656584
Ajay Pradhan <pradhanajay14@gmail.com>
2021-09-29 20:20:01 +0530

    commit

---
+++ Web-revision/q2.js
@@ -0,0 +1,9 @@
+let letFruit = "Orange";
+var varFruit = "Orange";
+console.log("letFruit", letFruit, "varFruit", varFruit);
+{
+  let letFruit = "apple";
+  var varFruit = "apple";
+  console.log("letFruit", letFruit, "varFruit", varFruit);
+}
+console.log("letFruit", letFruit, "varFruit", varFruit);
